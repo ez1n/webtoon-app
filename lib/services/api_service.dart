3885b395bb2f6ca03a4d10f9f1c1f1bf3b09b82e
ez1n <eodrmfdl1004@naver.com>
2024-02-19 17:30:33 +0900

    feat: 오늘의 웹툰 dto 추가 (model)

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = 'today';
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      List<WebtoonModel> webtoonInstances = [
+        for (var webtoon in webtoons) WebtoonModel.fromJson(webtoon),
+      ];
+      return webtoonInstances;
     }
     throw Error();
   }
